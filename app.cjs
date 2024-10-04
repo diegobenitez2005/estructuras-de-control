8ccb03c54079c2c9ac9e1070e4b7f06e3cc2542e
Diego <die10.2005@gmail.com>
2024-10-04 00:29:05 -0400

    primer commit

---
+++ app.cjs
@@ -0,0 +1,137 @@
+//VerificarParidad
+
+console.log("archivo funciona");
+
+function VerificarParidad(n){
+    if (n%2 === 0){console.log ("El numero es par");}
+    else {console.log ("El numero es impar");}
+}    
+
+ //clasificarEdad
+ function clasificarEdad(edad){
+    if (edad>18 && edad<65){console.log ("Es adulto");}
+    else if(edad>65){console.log ("Es adulto mayor"); }
+    else{console.log ("Es menor de edad");}
+
+ }
+
+ //cuentaRegresiva
+function cuentaRegresiva(n){
+   while(typeof n !== 'number' || n<=0 ){
+      console.log("El numero debe ser mayor a cero y entero");
+      return;
+   }
+   const cuentaRegresive= setInterval(()=>{
+      console.log(n);
+      n--;
+      if(n<0){
+         clearInterval(cuentaRegresive);
+         console.log("opa")
+         return;
+          
+      
+      }
+   
+  
+
+   },1000);
+}   
+
+
+//repetirMensaje
+function repetirMensaje(){
+   n=0
+   do{ 
+       console.log("Estoy aprendiendo JavScript");
+       n++;
+   }while(n<5);
+
+}
+//Repetir numeros pares
+let RepetirPares= function(n){
+   for(let i=0; i<n; i++){
+      
+      if(i%2 === 0 ){console.log(i);}
+   }
+
+}
+
+//detenerEnSeis
+function detenerEnSeis(){
+    const fin =10; 
+   for(let i=0; i<=fin; i++){
+      if(i===6){break;}
+   }
+}
+//saltar cinco
+function saltarCinco(){
+      fin=10;
+      for(let i=0; i<=fin;i++ ){
+         if (i===5){continue;}
+      }
+} 
+
+//esVocal
+function esVocal(vocal){
+   switch(vocal){
+      case "a":
+      case "e":
+      case "i":
+      case "o":
+      case "u":
+         return console.log("es una vocal");
+      default:
+         return console.log("no es una vocal")
+   }
+}
+
+//diaSemana
+function diaSemana(x){
+   switch(x){
+      case 1:
+         return console.log("lunes");
+      case 2:
+         return console.log("martes");
+      case 3:
+         return console.log("miercoles");
+      case 4:
+         return console.log("jueves");
+      case 5: 
+         return console.log("viernes");
+      case 6: 
+         return console.log("sabado");
+      case 7: 
+         return console.log("Domingo");
+   }
+
+}
+//evaluarNumeros
+function evaluarNumeros(a,b,c){
+   if(a>0 && b>0 && c>0){
+      console.log("SON TODOS POSITIVOS");
+
+   }
+   else if(a<0 && b<0 && c<0){
+      console.log("Son todos negativos");
+
+   }
+   else {
+      console.log("Al menos uno es negativo");
+   }
+   
+
+}
+ 
+ module.exports={
+    VerificarParidad,
+    clasificarEdad,
+    cuentaRegresiva,
+    repetirMensaje,
+    RepetirPares,
+    detenerEnSeis,
+    saltarCinco,
+    diaSemana,
+    esVocal,
+    evaluarNumeros,
+
+ };
